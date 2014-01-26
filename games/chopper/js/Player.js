53e61f3db4a09747c65c42b71f72fb7bf272a7b7
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-01-26 19:04:02 +0100

    Adding chopper spritsheet + player class file

---
+++ games/chopper/js/Player.js
@@ -0,0 +1,49 @@
+Player = function(game) {
+
+    this.game    = game;
+    this.sprite  = null;
+    this.cursors = null;
+};
+
+Player.prototype = {
+    preload: function() {
+        this.game.load.spritesheet('chopper', 'assets/sprites/chopper.png', 52, 36, 5);
+        this.game.load.spritesheet('mummy', 'assets/sprites/metalslug_mummy37x45.png', 37, 45, 18);
+    },
+
+    create: function() {
+        this.sprite = this.game.add.sprite(32, 100, 'chopper');
+
+        this.sprite.animations.add('flying');
+        this.sprite.animations.play('flying', 45, true);
+
+        this.sprite.body.collideWorldBounds = true;
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    },
+
+    update: function() {
+        this.game.physics.collide(this.sprite, level.layer);
+
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = 0;
+
+        if(this.cursors.left.isDown)
+        {
+            this.sprite.body.velocity.x = -250;
+        }
+        else if(this.cursors.right.isDown)
+        {
+            this.sprite.body.velocity.x = 250;
+        }
+
+        if(this.cursors.up.isDown)
+        {
+            this.sprite.body.velocity.y = -250;
+        }
+        else if(this.cursors.down.isDown)
+        {
+            this.sprite.body.velocity.y = 250;
+        }
+    }
+};
