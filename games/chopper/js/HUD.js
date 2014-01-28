1588fce304710a86056c5b9af6615496a16cd489
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-01-28 18:33:42 +0100

    Toggle button for sound working

--- games/chopper/js/HUD.js
+++ games/chopper/js/HUD.js
@@ -1,24 +1,17 @@
 HUD = function(game) {
 
-    this.game    = game;
-    this.soundToggle   = null;
+    this.game        = game;
+    this.soundToggle = null;
 };
 
 HUD.prototype = {
     preload: function() {
         this.game.load.spritesheet('button', 'assets/interface/sound.png', 16, 16);
-//        this.game.load.image('sound_on', 'assets/interface/sound_on.png');
-//        this.game.load.image('sound_off', 'assets/interface/sound_off.png');
     },
 
     create: function() {
-        this.soundToggle = this.game.add.button(this.game.world.width - 150, 15, 'button', this.toggleSound, this, 0, 1, 0);
-
-//        this.soundToggle = game.add.sprite(game.world.width - 150, 15, 'sound_on');
-//        this.soundToggle.inputEnabled=true;
-//        this.soundToggle.events.onInputDown.add(this.muteSound,this);
-//        this.soundToggle.events.onInputOver.add(this.overSoundToggle,this);
-//        this.soundToggle.events.onInputOut.add(this.outSoundToggle,this);
+        this.soundToggle = this.game.add.button(this.game.world.width - 150, 15, 'button', this.toggleSound, this);
+        this.soundToggle.frame = 1;
     },
 
     update: function() {
@@ -28,25 +21,15 @@ HUD.prototype = {
         this.game.debug.renderSpriteBody(this.soundToggle);
     },
 
-    overSoundToggle: function()
-    {
-        this.game.stage.canvas.style.cursor = "pointer";
-    },
-
-    outSoundToggle: function()
-    {
-        this.game.stage.canvas.style.cursor = "default";
-    },
-
     toggleSound: function()
     {
         if (this.game.sound._muted) {
             this.game.sound.mute   = false;
-//            this.soundToggle.loadTexture('sound_on', 0);
+            this.soundToggle.frame = 1;
         }
         else {
             this.game.sound.mute   = true;
-//            this.soundToggle.loadTexture('sound_off', 0);
+            this.soundToggle.frame = 0;
         }
     }
 };
