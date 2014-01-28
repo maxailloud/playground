2e836e054c60a1645a29fca0a94b93d8b3c7d19d
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-01-28 13:52:55 +0100

    Creating game when window.onload

--- games/chopper/js/game.js
+++ games/chopper/js/game.js
@@ -1,43 +1,45 @@
-var game = new Phaser.Game(720, 450, Phaser.CANVAS, 'canvas-container', { preload: preload, create: create, update: update , render: render });
+window.onload = function() {
+    var game = new Phaser.Game(720, 450, Phaser.CANVAS, 'canvas-container', { preload: preload, create: create, update: update , render: render });
 
-var player = null;
-var level = null;
-var hud = null;
+    var player = null;
+    var level = null;
+    var hud = null;
 
-var button;
+    var button;
 
-function preload()
-{
-    level = new Level(game);
-    level.preload();
+    function preload()
+    {
+        level = new Level(game);
+        level.preload();
 
-    player = new Player(game);
-    player.preload();
+        player = new Player(game);
+        player.preload();
 
-    hud = new HUD(game);
-    hud.preload();
-}
+        hud = new HUD(game);
+        hud.preload();
+    }
 
-function create()
-{
-    level.create();
-    player.create();
-    hud.create();
-}
+    function create()
+    {
+        level.create();
+        player.create();
+        hud.create();
+    }
 
-function update()
-{
-    level.update();
-    player.update();
-    hud.update();
-}
+    function update()
+    {
+        level.update();
+        player.update();
+        hud.update();
+    }
 
-function render()
-{
-    game.debug.renderText("x : " + game.input.mousePointer.x, 32, 410);
-    game.debug.renderText("y : " + game.input.mousePointer.y, 32, 430);
-    game.debug.renderText("Sound " + !game.sound._muted, 150, 410);
+    function render()
+    {
+        game.debug.renderText("x : " + game.input.mousePointer.x, 32, 410);
+        game.debug.renderText("y : " + game.input.mousePointer.y, 32, 430);
+        game.debug.renderText("Sound " + !game.sound._muted, 150, 410);
 
-    level.render();
-    hud.render();
-}
+        level.render();
+        hud.render();
+    }
+};
