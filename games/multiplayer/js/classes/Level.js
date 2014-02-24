f3fce835b1c3d753fc2d18a3db6a0768474ec803
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-02-24 13:12:23 +0100

    Display point (later) animated circle when clicking on canvas

--- games/multiplayer/js/classes/Level.js
+++ games/multiplayer/js/classes/Level.js
@@ -1,16 +1,27 @@
 Multiplayer.Level = function(game) {
 
     this.game = game;
+    this.point;
 };
 
 Multiplayer.Level.prototype = {
     create: function() {
         this.game.stage.backgroundColor = "#79A8FF";
+
+        this.game.input.onDown.add(this.displayMousePosition, this);
+
+        this.point = new Phaser.Point(this.game.world.centerX, this.game.world.centerY);
     },
 
     update: function() {
     },
 
     render: function() {
+        this.game.debug.renderPoint(this.point, 'red');
+    },
+
+    displayMousePosition: function(pointer, event) {
+        this.point.x = pointer.x;
+        this.point.y = pointer.y;
     }
 };
