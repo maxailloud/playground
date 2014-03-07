cf545c896fddc65c7ec35506c62151c3f8694f1a
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-03-07 12:20:51 +0100

    Add some sugar for the interface

--- games/multiplayer/js/states/Boot.js
+++ games/multiplayer/js/states/Boot.js
@@ -11,27 +11,28 @@ Multiplayer.Boot.prototype = {
     },
 
     create: function() {
-        this.game.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
+        this.input.keyboard.addKeyCapture(Phaser.Keyboard.SPACEBAR);
 
-        this.game.input.maxPointers = 1;
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = true;
 
         if(this.game.device.desktop)
         {
             //If you have any desktop specific settings, they can go in here
-            this.game.stage.scale.pageAlignHorizontally = true;
+            this.stage.scale.pageAlignHorizontally = true;
         }
         else
         {
             //Same goes for mobile settings.
             //In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-            this.game.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
-            this.game.stage.scale.minWidth = 480;
-            this.game.stage.scale.minHeight = 260;
-            this.game.stage.scale.maxWidth = 1024;
-            this.game.stage.scale.maxHeight = 768;
-            this.game.stage.scale.forceLandscape = true;
-            this.game.stage.scale.pageAlignHorizontally = true;
-            this.game.stage.scale.setScreenSize(true);
+            this.stage.scaleMode = Phaser.StageScaleMode.SHOW_ALL;
+            this.stage.scale.minWidth = 480;
+            this.stage.scale.minHeight = 260;
+            this.stage.scale.maxWidth = 1024;
+            this.stage.scale.maxHeight = 768;
+            this.stage.scale.forceLandscape = true;
+            this.stage.scale.pageAlignHorizontally = true;
+            this.stage.scale.setScreenSize(true);
         }
 
         //By this point the preloader assets have loaded to the cache, we've set the game settings
--- games/multiplayer/js/states/Preloader.js
+++ games/multiplayer/js/states/Preloader.js
@@ -1,21 +1,27 @@
 Multiplayer.Preloader = function(game) {
     this.background = null;
-    this.preloadBar = null;
+    this.loadingBar = null;
 };
 
 Multiplayer.Preloader.prototype = {
 
     preload: function() {
         this.background = this.add.sprite(0, 0, 'preloaderBackground');
-        this.preloadBar = this.add.sprite(this.world.width - (this.world.width / 2) - 150, 50, 'preloaderBar');
+        this.loadingBar = this.add.sprite(this.world.width - (this.world.width / 2) - 150, 50, 'preloaderBar');
 
-        this.load.setPreloadSprite(this.preloadBar);
+        this.load.setPreloadSprite(this.loadingBar);
 
         this.game.load.image('user', 'assets/sprites/user.png');
     },
 
     create: function() {
-        this.game.state.start('MainMenu');
+        var tween = this.add.tween(this.loadingBar)
+            .to({alpha: 0}, 1000, Phaser.Easing.Linear.None, true);
+        tween.onComplete.add(this.startGame, this);
+    },
+
+    startGame: function() {
+        this.game.state.start('Game');
     }
 
 };
