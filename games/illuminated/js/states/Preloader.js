98136957597f31c1814dae8541c09882cd68597e
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-03-19 12:47:53 +0100

    Remove evrything for illuminated test

--- games/illuminated/js/states/Preloader.js
+++ games/illuminated/js/states/Preloader.js
@@ -10,11 +10,6 @@ Illuminated.Preloader.prototype = {
         this.loadingBar = this.add.sprite(this.world.width - (this.world.width / 2) - 150, 50, 'preloaderBar');
 
         this.load.setPreloadSprite(this.loadingBar);
-
-        this.game.load.image('car', 'assets/sprites/car.png');
-
-        this.game.load.tilemap('desert', 'assets/maps/desert.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.image('tiles', 'assets/tiles/tmw_desert_spacing.png');
     },
 
     create: function() {
